faa0bd9848dba9a610bb15fc4cbb7a1abebc1060
Imane Younsi <imane6younsi@gmail.com>
2026-06-21 15:47:56 +0100

    version finale

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,19 +1,26 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail; 
+
 use Laravel\Sanctum\HasApiTokens;
+
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use NotificationChannels\WebPush\HasPushSubscriptions;
-use App\Notifications\VerifyEmailBrevo;
+use NotificationChannels\WebPush\HasPushSubscriptions; // ← AJOUTÉ
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail // ← AJOUTÉ
 {
-    use HasApiTokens, HasFactory, Notifiable, HasPushSubscriptions;
+    use HasApiTokens, HasFactory, Notifiable, HasPushSubscriptions; // ← HasPushSubscriptions ajouté
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
         'name',
         'prenom',
@@ -25,21 +32,26 @@ class User extends Authenticatable implements MustVerifyEmail
         'genre',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function sendEmailVerificationNotification()
-    {
-        $this->notify(new VerifyEmailBrevo());
     }
 }
